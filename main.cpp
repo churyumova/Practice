6084e8ab2884a4e2aadcf5f7cee7000f49a85582
polina <polina.churyumova@mail.ru>
2023-02-17 10:31:14 +0300

    ghgjgj

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>;
+
+int main(){
+	std::string first_name, last_name;
+	std::cout << "Enter your first name: ";
+	std::cin >> first_name;
+	std::count << "Now enter your last name: ";
+	std::cin >> last_name;
+	std::count <<"Hello, "<< first_name << " " << last_name << "!";
+}
